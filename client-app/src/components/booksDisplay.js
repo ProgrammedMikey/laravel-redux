0d5e3b6c5b7e3d374d53e723c31efb3b9632995a
Michael DaSilva <michaeldasilva@Michaels-MacBook-Pro.local>
2016-12-29 12:36:13 -0500

    setting up project

---
+++ client-app/src/components/booksDisplay.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+import BookList from '../containers/book-list';
+import BookDetail from '../containers/book-detail';
+
+export default () => <div>
+    <div>
+        <BookList />
+        <BookDetail />
+    </div>
+
+</div>
---
+++ client-app/src/components/welcome.js
@@ -0,0 +1,55 @@
+import React,{Component} from 'react';
+import Posts from './posts/posts';
+
+export default () => <div>
+        <header className="jumbotron hero-spacer">
+                <h1>Book Trading Club</h1>
+                <p>First rule of Book Trading Club is don't talk about Book trading Club!</p>
+        </header>
+
+
+        <div className="row">
+                <div className="col-lg-12">
+                        <h3>Latest Features</h3>
+                </div>
+        </div>
+        <hr/>
+
+        <div className="row text-center">
+
+                <div className="col-md-3 col-sm-6 hero-feature">
+                        <div className="thumbnail">
+                                        <div className="caption">
+                                                <p>
+                                                        Catalogue your books online.</p>
+                                        </div>
+                        </div>
+                </div>
+
+                <div className="col-md-3 col-sm-6 hero-feature">
+                        <div className="thumbnail">
+                                        <div className="caption">
+                                                <p>See all books our user own.</p>
+                                        </div>
+                        </div>
+                </div>
+
+                <div className="col-md-3 col-sm-6 hero-feature">
+                        <div className="thumbnail">
+                                        <div className="caption">
+                                                <p>Request to borrow other users books.</p>
+                                        </div>
+                        </div>
+                </div>
+
+                <div className="col-md-3 col-sm-6 hero-feature">
+                        <div className="thumbnail">
+                                        <div className="caption">
+                                                <p>Easily manage books and requests from your dashboard.</p>
+                                        </div>
+                        </div>
+                </div>
+
+        </div>
+        <Posts />
+</div>
